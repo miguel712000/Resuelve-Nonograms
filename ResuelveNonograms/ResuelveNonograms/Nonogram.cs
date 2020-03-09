6b96841cee46140dd075b6083e585acde1e668a9
your name <you@example.com>
2020-03-09 03:13:41 -0600

    Crea y lee parcialmente nonogramas
    Etapa temprana, aun no lee la lista de los nonogramas, se esta buscando el error

---
+++ ResuelveNonograms/ResuelveNonograms/Nonogram.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResuelveNonograms
+{
+    class Nonogram
+    {
+        private int filas, columnas;
+        private Dictionary<int,List<int>> pistasFilas, pistasColumnas;
+
+
+
+        public Nonogram(int filas, int columnas, Dictionary<int, List<int>> pistasFilas, Dictionary<int, List<int>> pistasColumnas)
+        {
+            this.filas = filas;
+            this.columnas = columnas;
+            this.pistasFilas = pistasFilas;
+            this.pistasColumnas = pistasColumnas;
+
+        }
+
+        public void estableceFilas(int filas)
+        {
+            this.filas = filas;
+        }
+
+        public void estableceColumnas(int columnas)
+        {
+            this.columnas = columnas;
+        }
+
+        public void establecePistasFilas(Dictionary<int, List<int>> pistasFilas)
+        {
+            this.pistasFilas = pistasFilas;
+        }
+
+        public void establecePistasColumnas(Dictionary<int, List<int>> pistasColumnas)
+        {
+            this.pistasColumnas = pistasColumnas;
+        }
+
+        public String dameNonogram()
+        {
+            String texto;
+
+            texto = filas + ", " + columnas + "\nFilas\n";
+
+            for(int i = 0; i < pistasFilas.Count - 1; i++)
+            {
+                foreach(int pista in pistasFilas[i])
+                {
+                    texto += pista + ", ";
+                }
+            }
+
+            texto += "\nColumnas\n";
+
+            for (int i = 0; i < pistasColumnas.Count - 1; i++)
+            {
+                foreach (var pista in pistasColumnas[i])
+                {
+                    Console.WriteLine(pista);
+                    texto += pista + ", ";
+                }
+            }
+
+            return texto;
+        }
+        
+    }
+}
---
+++ ResuelveNonograms/ResuelveNonograms/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ResuelveNonograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, List<int>> pistasFilas = new Dictionary<int, List<int>>();
+            Dictionary<int, List<int>> pistasColumnas = new Dictionary<int, List<int>>();
+
+            List<int> digitos = new List<int>();
+            digitos.Add(2);
+            digitos.Add(3);
+
+            pistasFilas.Add(0, digitos);
+            digitos.Clear();
+
+            digitos.Add(4);
+            digitos.Add(5);
+
+            pistasFilas.Add(1, digitos);
+            digitos.Clear();
+
+            
+
+            digitos.Add(1);
+            digitos.Add(1);
+
+            pistasColumnas.Add(0, digitos);
+            digitos.Clear();
+
+            pistasColumnas.Add(1, digitos);
+            digitos.Clear();
+
+            digitos.Add(2);
+            digitos.Add(2);
+            digitos.Add(2);
+
+            pistasColumnas.Add(2, digitos);
+            digitos.Clear();
+
+            {
+                Nonogram puzle = new Nonogram(2, 3, pistasFilas, pistasColumnas);
+
+                Console.WriteLine(puzle.dameNonogram());
+            }
+        }
+    }
+}
