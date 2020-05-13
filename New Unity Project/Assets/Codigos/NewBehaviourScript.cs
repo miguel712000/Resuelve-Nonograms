34d151a5f49b2b48843919db0ae961723e735827
fabianmartinez04 <54187391+fabianmartinez04@users.noreply.github.com>
2020-05-13 17:36:06 -0600

    Parte de la Interfaz
    Tuve muchos problemas con el git, hasta ahora subo esto pero ya estaba listo hace tiempo

---
+++ New Unity Project/Assets/Codigos/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    string path;
+
+    public void AbrirExplorador()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with png","","txt");
+        StreamReader sr = new StreamReader(path);
+        sr.BaseStream.Seek(0, SeekOrigin.Begin);
+        string str = sr.ReadLine();
+        while (str != null)
+        {
+            str = sr.ReadLine();
+        }
+
+        // to close the stream 
+        sr.Close();
+    }
+
+
+}
---
+++ New Unity Project/Assets/Scenes/cambioescena.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class cambioescena : MonoBehaviour
+{
+    public void cambio()
+    {
+        SceneManager.LoadScene("Scene2");
+
+    }
+}
