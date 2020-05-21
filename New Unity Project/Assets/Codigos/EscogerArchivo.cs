589d7e38ac58bf2be73a37c15a7cc893eb06babc
fabianmartinez04 <54187391+fabianmartinez04@users.noreply.github.com>
2020-05-20 23:31:12 -0600

    Cambios detalles

---
+++ New Unity Project/Assets/Codigos/EscogerArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+
+public class EscogerArchivo : MonoBehaviour
+{
+    string path;
+    int filas, columnas;
+    int[][] pistasFilas;
+    int[][] pistasColumnas;
+
+    public void AbrirExplorador()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with png","","txt");
+   
+    }
+
+    public void leerArchivo()
+    {
+        
+    }
+
+
+}
